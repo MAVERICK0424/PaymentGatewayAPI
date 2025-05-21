8a23dc3bfa485c2f7cd67a7679ee202578d7428c
HomePC <HomePC@TEEJAY>
2025-05-21 11:05:43 +0100

    payment changes

---
+++ PaystackSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentGatewayAPI.Settings
+{
+    public class PaystackSettings
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public string BaseUrl { get; set; } = string.Empty;
+    }
+}
